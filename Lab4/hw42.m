213a4f78f145994bcc44764097730e0e24acec04
BeardedRoar <joeljoel224@hotmail.com>
2017-02-23 13:41:54 +0100

    made the picture look better

--- Lab4/hw42.m
+++ Lab4/hw42.m
@@ -6,20 +6,20 @@ P = cvpartition(Y,'Holdout',0.20);
 cf = @(x)([(x==1) 0 (x==-1)]);
 col = table2array(rowfun(cf,table(Y)));
 
-svmStruct = svmtrain(X,Y,'boxconstraint',3,'showplot', false);
+svmStruct = svmtrain(X,Y,'boxconstraint',3,'showplot', true, 'autoscale',false);
 
-figure
-scatter(X(:,1),X(:,2),40,col); hold on;
+figure; hold on;
+scatter(X(:,1),X(:,2),40,col);
 
 svi = svmStruct.SupportVectorIndices;
-scatter(X(svi,1),X(svi,2),40,'k+'); hold on;
+scatter(X(svi,1),X(svi,2),40,'k+');
 
 w1 = dot(svmStruct.Alpha, X(svi,1));
 w2 = dot(svmStruct.Alpha, X(svi,2));
 bias = svmStruct.Bias;
 f = @(x)-(w1/w2)*x-bias/w2;
 
-x = linspace(-5,5,1000);
+x = linspace(min(X(:,1)),max(X(:,1)),1000);
 plot(x,f(x),'k');
 %%
 svm = fitcsvm(X,Y,'CrossVal','off');
