ba85dee5045df79ca150bf0b8ea6f7ae47efab6b
BeardedRoar <joeljoel224@hotmail.com>
2017-02-26 14:24:43 +0100

    fixed figure for 2.1

--- Lab4/hw42.m
+++ Lab4/hw42.m
@@ -1,33 +1,38 @@
 clear all; close all; clc;
 load d1b.mat;
-P = cvpartition(Y,'Holdout',0.20);
 
-
-cf = @(x)([(x==1) 0 (x==-1)]);
-cf2 = @(x)([(x==1) 1 (x==-1)]);
-col = table2array(rowfun(cf,table(Y)));
-
+% ---Calculations----------------------------
+% Training the svm, and findng the missclassified points
 svmStruct = svmtrain(X,Y,'boxconstraint',1,'showplot', false, 'autoscale',false);
-
-figure; hold on;
-scatter(X(:,1),X(:,2),40,col);
-
 species = svmclassify(svmStruct, X);
-col2 = cf2(X(Y~=species));
-scatter(X(Y~=species,1),X(Y~=species,2),100,col2);
 
-svi = svmStruct.SupportVectorIndices;
-scatter(X(svi,1),X(svi,2),40,'k+');
-
+% Calulating the hyperplane
 w1 = dot(svmStruct.Alpha, X(svi,1));
 w2 = dot(svmStruct.Alpha, X(svi,2));
 bias = svmStruct.Bias;
 f = @(x)-(w1/w2)*x-bias/w2;
 
+% Calculating the soft margin
+softmargin = 2/sqrt(w1*w1+w2*w2);
+
+%---Plotting---------------------------------
+fig = figure; hold on;
+% Plotting the points
+gscatter(X(:,1),X(:,2),Y,'br','x',7);
+
+% Marking the misscalculated points
+scatter(X(Y~=species,1),X(Y~=species,2),80,'go');
+
+% Marking the support vectors
+svi = svmStruct.SupportVectorIndices;
+scatter(X(svi,1),X(svi,2),particleSize,'k+');
+
+% Plotting the hyperplane
 x = linspace(min(X(:,1)),max(X(:,1)),1000);
 plot(x,f(x),'k');
 
-softmargin = 2/sqrt(w1*w1+w2*w2);
-%%
-svm = fitcsvm(X,Y,'CrossVal','off');
-CVSVMModel = crossval(svm);
+% Adding legend
+legend('Y = 1','Y = -1','missclassified','support vector','hyperplane');
+
+saveas(fig,'hw4-2-1.png');
+hold off;
